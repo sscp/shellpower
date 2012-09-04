7c8a8c1192c2104f234ce07056568040cc90cbae
DC <dcposch@dcpos.ch>
2012-09-04 03:00:50 -0700

    Started unit tests for geometry calculations

---
+++ src/ShellPowerTest/GeomTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTK;
+
+namespace SSCP.ShellPower.Test {
+    [TestClass]
+    public class GeomTest {
+        [TestMethod]
+        public void TestPolygon() {
+            Polygon2 poly = new Polygon2() {
+                vertices = new Vector2[]{
+                    new Vector2(1, 1),
+                    new Vector2(2, 1),
+                    new Vector2(1, 2)
+                }
+            };
+            Assert.IsTrue(poly.Contains(new Vector2(1.1f, 1.1f)));
+            Assert.IsTrue(poly.Contains(new Vector2(1.1f, 1.8f)));
+            Assert.IsTrue(poly.Contains(new Vector2(1.49f, 1.49f)));
+            Assert.IsFalse(poly.Contains(new Vector2(1.51f, 1.51f)));
+            Assert.IsFalse(poly.Contains(new Vector2(0f, 0f)));
+            Assert.IsTrue(poly.Contains(new Vector2(1f, 1f)));
+        }
+    }
+}
