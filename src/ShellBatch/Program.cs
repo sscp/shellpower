7ab2f1f9160782e6453de58500a8f3d56d5966e3
DC <dan@addepar.com>
2014-02-17 20:56:56 -0800

    ShellBatch (command-line), work in progress

---
+++ src/ShellBatch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SSCP.ShellPower {
+    class Program {
+        static void Main(string[] args) {
+            ArraySimulationStepInput input = new ArraySimulationStepInput();
+            input.Array = new ArraySpec();
+
+            Console.WriteLine("hello world");
+        }
+    }
+}
